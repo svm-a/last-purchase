50e14c8098dd715b6f855fcc5ff50b431ffbb980
Max Nowak <spearekurwa@yandex.com>
2023-01-12 19:34:41 +0300

    update Main, change sdk, add money file
    for some operations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,49 +1,20 @@
-import kotlin.system.exitProcess
-
 fun main() {
-	println(showTips())
-//	println(showStartMenu())
-//	println(stepSelection())
+	println(showTable())
 }
 
-fun showStartMenu(): String {
-
+fun showTable(): String {
 	return """
-	Welcome to your favorite store in your favorite city.
-	I suggest you take advantage of small tips on how to be in our store.
-
-	1. Write the number 1 if you want to see these hints.
-	2. Write the number 2 if you want to go further and ignore the prompts.
-	3. Write the number 3 If you want to leave.
+		When entering the store, you pay attention to the location sign to your left. 
+		Have you noticed this?
+		
+		"For new visitors to the store": 
+			1. Buy a product.
+			2. If you want to take more up to 45 products - take a cart.
+			3. If you want to take more up to 15 products - take a basket.
+			4. If you want to take more up to 5 products - take your hands.
+		
 	""".trimIndent()
 }
 
-fun getOpinionFromUser(): Int {
-	val value = readln().toInt()
-	return value
-}
 
-fun stepSelection() {
-	when (getOpinionFromUser()) {
-		1 -> println(showTips())
-		2 -> println(runShop())
-		3 -> exitProcess(0)
-
-	}
-}
-
-fun runShop(): String {
-	return "start shop"
-}
-
-fun showTips(): String {
-	return """
-	As in any store, you can take a basket, a cart or nothing. In each case there will be peculiar restrictions.
-	
-	1. If you take a cart, you will be able to take no more than 40 items.
-	2. If you take the basket. You will only be able to take 15 items.
-	3. If you don't take anything, you can only take 5 items.
-	
-	""".trimIndent()
-}
 
