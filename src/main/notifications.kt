c1d1cc97156dff871c066b1d03eae9d687d04133
Max Nowak <spearekurwa@yandex.com>
2023-01-13 15:14:02 +0300

    add func to notify the user

---
+++ src/main/notifications.kt
@@ -0,0 +1,9 @@
+fun firstNotification(): String {
+	return """
+		*** You have received a notification. ***
+
+		Write 1 if you want to buy products
+		Write 2 if you want to leave the store.
+		
+	""".trimIndent()
+}
