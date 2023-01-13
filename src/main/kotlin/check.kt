989f6e8d37cadd194d7a29a2d958b9e9fc7244f4
Max Nowak <spearekurwa@yandex.com>
2023-01-13 12:01:38 +0300

    update check
    1. if the value does not match, then he will call the function again and it will be infinite until he enters the correct data.
    2. we pass the correct data to the main function, where it will call the corresponding functions (either it's a shop or it's a casino)

--- src/main/kotlin/check.kt
+++ src/main/kotlin/check.kt
@@ -1,13 +1,15 @@
-fun checkFirstAnswer(): Boolean {
+fun checkFirstAnswer(): String {
+	println("write a value")
 	val value = readln()
 
 	when (value) {
-		"1", "2" -> {
-			return true
-		}
+		"1" ->
+			return "1"
 
-		else -> {
-			return false
-		}
+		"2" ->
+			return "2"
+
+		else ->
+			return checkFirstAnswer() //
 	}
 }
