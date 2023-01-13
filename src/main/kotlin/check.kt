996306d71e50dcf02d980afb7d7db6597034055c
Max Nowak <spearekurwa@yandex.com>
2023-01-13 12:25:14 +0300

    update text info

--- src/main/kotlin/check.kt
+++ src/main/kotlin/check.kt
@@ -1,15 +1,20 @@
 fun checkFirstAnswer(): String {
-	println("write a value")
+	println(
+		"""
+		//////////////////////////
+		/  Write a text message  /
+		/		 1 or 2          /
+		//////////////////////////
+	""".trimIndent()
+	)
+	print("write here > ")
 	val value = readln()
 
 	when (value) {
-		"1" ->
-			return "1"
+		"1" -> return "1"
 
-		"2" ->
-			return "2"
+		"2" -> return "2"
 
-		else ->
-			return checkFirstAnswer() //
+		else -> return checkFirstAnswer() //
 	}
 }
